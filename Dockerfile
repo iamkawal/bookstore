f93f74a0776795bcb93671568135b012c0298a7d
Kawaljit Singh <kawaljit.s@outlook.com>
2022-03-23 21:03:06 -0700

    redis working + integrate ejs

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,17 @@
-FROM node:lts
+#Specify a base image
+FROM node:alpine
 
-WORKDIR '/var/www/app'
+#Specify a working directory
+WORKDIR /usr/app
+
+#Copy the dependencies file
+COPY ./package.json ./
+
+#Install dependencies
+RUN npm install 
+
+#Copy remaining files
+COPY ./ ./
+
+#Default command
+CMD ["npm","start"]
--- app.js
+++ app.js
@@ -2,21 +2,35 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// (async () => {
-//     const cache = createClient();
-//     cache.on('error', (err) => console.log('Redis client couldn\'t start: ', err))
-    
-//     await cache.connect();
-// })()
 
-const PORT = process.env.PORT || 3000
+app.set('view engine', 'ejs');
+let redisClient;
 
+(async () => {
+	redisClient = redis.createClient({url: process.env.REDIS_URL})
+	redisClient.on('error', (err) => console.log('Redis Client Error', err));
+	await redisClient.connect();
+	await redisClient.set('hello', 'there');
+	const value = await redisClient.get('hello');
+	console.log('value is : ', value);
+})();
 
 app.get('/', async (req, res) => {
-    return res.send('<p>Hello there</p>')
-})
+	res.render('index')
 
+});
+
+
+app.get('/:id', async(req, res) => {
+	const {id} = req.params;
+	console.log('id is', id)
+	const rawData = await redisClient.get(id);
+
+	console.log('rawData is : ', rawData)
+	return res.send(rawData)
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
