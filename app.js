552765eb697102896a92d136876ef198b8347240
Kawaljit Singh <kawaljit.s@outlook.com>
2022-03-23 23:54:58 -0700

    handle different routes properly

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const redis = require('redis');
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.set('view engine', 'ejs');
-let redisClient;
+app.use(bodyParser.urlencoded({ extended: true }))
 
+let redisClient;
 (async () => {
 	redisClient = redis.createClient({url: process.env.REDIS_URL})
 	redisClient.on('error', (err) => console.log('Redis Client Error', err));
 	await redisClient.connect();
-	await redisClient.set('hello', 'there');
-	const value = await redisClient.get('hello');
-	console.log('value is : ', value);
 })();
+
+app.get('/admin', async(req, res) => {
+	let orders = []
+	for await (const orderWithScore of redisClient.zScanIterator('orders')) {
+		// order = JSON.parse(orderWithScore)
+		orders.push(JSON.parse(orderWithScore.value))
+	}
+
+	return res.render('orders', {orders : orders});
+})
+
+app.post('/success', async(req, res) => {
+	const timestamp = new Date()
+	let order  = {timestamp: timestamp.getTime(), bookName: req.body.bookName}
+	await redisClient.zAdd('orders', [{score: timestamp.getTime(),  value: JSON.stringify(order) }])
+	return res.render('order-success')
+});
 
 app.get('/', async (req, res) => {
 	res.render('index')
 
-});
-
-
-app.get('/:id', async(req, res) => {
-	const {id} = req.params;
-	console.log('id is', id)
-	const rawData = await redisClient.get(id);
-
-	console.log('rawData is : ', rawData)
-	return res.send(rawData)
 });
 
 app.listen(PORT, () => {
